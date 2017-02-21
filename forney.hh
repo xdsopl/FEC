c329e234428baf24a74a0e22d864edb3e18fecb9
Ahmet Inan <xdsopl@gmail.com>
2017-02-21 15:02:37 +0100

    renamed to FEC to make room for BCH

--- forney.hh
+++ forney.hh
@@ -1,5 +1,5 @@
 /*
-reedsolomon - Reed-Solomon error correction
+FEC - Forward error correction
 Written in 2017 by <Ahmet Inan> <xdsopl@gmail.com>
 To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
 You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
